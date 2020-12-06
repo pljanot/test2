ede045640b98213bde55bddf35ab08728ff1f0e1
pljanot <pat129@wp.pl>
2020-12-06 17:44:01 +0100

    Add files via upload
    testes

---
+++ test_file_c#_class.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Class1
+{
+	public Class1()
+	{
+        string some_string = "hello world";
+        Int64 big_int = 1111111111;
+        List<string> string_array = new List<string>();
+	}
+}
